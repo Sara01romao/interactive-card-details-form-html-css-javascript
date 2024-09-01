893bbae44f3193090fdc78474468b16853da4656
Sara Romao <sara-romao@live.com>
2024-09-01 11:07:09 -0300

    feat: melhoria da validação dos campos e add msg de erro

--- script.js
+++ script.js
@@ -1,3 +1,197 @@
+
+document.getElementById('cardForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    let valid = true;
+
+    // Validação do nome
+    const name = document.getElementById('cardName').value.trim();
+    const inputName = document.getElementById('cardName');
+
+    if (name === '' || name.length < 5) {
+        document.getElementById('nameError').textContent = 'Can’t be blank';
+        inputName.classList.add('activeErro');
+        valid = false;
+    } else {
+        document.getElementById('nameError').textContent = '';
+    }
+
+    // Validação do número do cartão
+    const cardNumber = document.getElementById('cardNumber').value.replace(/\s/g, '');
+    const inputNumber = document.getElementById('cardNumber');
+    const cardNumberPattern = /^[0-9]{16}$/;
+
+    if (!cardNumberPattern.test(cardNumber)) {
+        document.getElementById('numberError').textContent = 'Wrong format, numbers only';
+        inputNumber.classList.add('activeErro');
+        valid = false;
+    } else {
+        document.getElementById('numberError').textContent = '';
+    }
+
+    // Validação da data de expiração
+    const month = document.getElementById('cardMonth').value.trim();
+    const inputMonth = document.getElementById('cardMonth');
+    const year = document.getElementById('cardYear').value.trim();
+    const inputYear = document.getElementById('cardYear');
+
+
+    if ((month === '' || year === '') ||(month === '00' || year === '00') || (month.length < 2 || year.length < 2)) {
+       
+        inputMonth.classList.add('activeErro');
+        inputYear.classList.add('activeErro');
+        document.getElementById('dateError').textContent = 'Invalid date';
+        valid = false;
+    } else if (month && year) {
+        
+        inputMonth.classList.remove('activeErro');
+        inputYear.classList.remove('activeErro');
+        document.getElementById('dateError').textContent = ''; 
+        valid = true;
+    }
+
+    // Validação do CVC
+    const cvc = document.getElementById('cardCvc').value.trim();
+    const cvcPattern = /^[0-9]{3}$/;
+    const inputCvc = document.getElementById('cardCvc');
+
+    if (!cvcPattern.test(cvc) ) {
+        document.getElementById('cvcError').textContent = 'Can’t be blank';
+        inputCvc.classList.add('activeErro');
+        valid = false;
+    } else {
+        document.getElementById('cvcError').textContent = '';
+        inputCvc.classList.add('activeErro');
+    }
+
+    // Se tudo estiver válido, você pode continuar com o processamento do formulário
+    if (valid) {
+     
+        document.querySelector('.thank-container').style.display = 'flex';
+        document.querySelector('#cardForm').style.display = 'none';
+        clear()
+    }
+});
+
+function clear(){
+    document.getElementById('cardNumberTxt').innerHTML = '0000 0000 0000 0000';
+    document.getElementById('cardNameTxt').innerHTML = 'JANE APPLESEED';
+    document.getElementById('cardMonthTxt').innerHTML = '00';
+    document.getElementById('cardYearTxt').innerHTML = '00';
+    document.getElementById('cardCvcTxt').innerHTML = '000';
+
+}
+
+// Função de validação para campos individuais
+function validateField(fieldId, errorId, validationFn) {
+    const field = document.getElementById(fieldId);
+    const errorElement = document.getElementById(errorId);
+
+    if (validationFn(field.value.trim())) {
+        errorElement.textContent = '';
+    } else {
+        errorElement.textContent = 'Invalid input';
+    }
+}
+
+document.getElementById('cardName').addEventListener('blur', function() {
+    const name = this.value;
+    const nextInput = this.nextElementSibling; 
+
+    if (name === '' || name.length < 5) {
+        this.classList.add('activeErro');
+        document.getElementById('nameError').textContent = 'Can’t be blank';
+        
+        if (nextInput && nextInput.tagName === 'INPUT') {
+            nextInput.classList.add('activeErro');
+        }
+    } else {
+        this.classList.remove('activeErro');
+        document.getElementById('nameError').textContent = '';
+    }
+});
+
+document.getElementById('cardNumber').addEventListener('blur', function() {
+    const cardNumber = this.value.replace(/\s/g, '');
+    const cardNumberPattern = /^[0-9]{16}$/;
+    const nextInput = this.nextElementSibling; 
+
+    if (!cardNumberPattern.test(cardNumber)) {
+        this.classList.add('activeErro');
+        document.getElementById('numberError').textContent = 'Wrong format, numbers only';
+
+        if (nextInput && nextInput.tagName === 'INPUT') {
+            nextInput.classList.add('activeErro');
+        }
+    } else {
+        this.classList.remove('activeErro');
+        document.getElementById('numberError').textContent = '';
+    }
+});
+
+document.getElementById('cardMonth').addEventListener('blur', function() {
+    const month = this.value;
+    const nextInput = this.nextElementSibling; 
+    
+    if (month === '' || month.length < 2 || month === "00") {
+        this.classList.add('activeErro');
+        document.getElementById('dateError').textContent = 'Can’t be blank';
+
+        if (nextInput && nextInput.tagName === 'INPUT') {
+            nextInput.classList.add('activeErro');
+        }
+    } else {
+        this.classList.remove('activeErro');
+        document.getElementById('dateError').textContent = '';
+    }
+});
+
+document.getElementById('cardYear').addEventListener('blur', function() {
+    const year = this.value;
+    const inputYear = document.getElementById('cardYear'); 
+
+    if (year === '' ||  year.length < 2 || year ==="00") {
+        inputYear.classList.add('activeErro');
+        document.getElementById('dateError').textContent = 'Can’t be blank';
+
+        if (nextInput && nextInput.tagName === 'INPUT') {
+            inputYear.classList.add('activeErro');
+        }
+    } else {
+        inputYear.classList.remove('activeErro');
+        document.getElementById('dateError').textContent = '';
+    }
+});
+
+document.getElementById('cardCvc').addEventListener('blur', function() {
+    const cvc = this.value;
+    const cvcPattern = /^[0-9]{3}$/;
+    const inputCvc = document.getElementById('cardCvc'); 
+
+    if (!cvcPattern.test(cvc)) {
+        inputCvc.classList.add('activeErro');
+        document.getElementById('cvcError').textContent = 'Can’t be blank';
+
+        if (nextInput && nextInput.tagName === 'INPUT') {
+            inputCvc.classList.add('activeErro');
+        }
+    } else {
+        inputCvc.classList.remove('activeErro');
+        document.getElementById('cvcError').textContent = '';
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 function atualizarElementoComInput(inputValue, elementId) {
     const element = document.getElementById(elementId);
     console.log(elementId)
@@ -55,14 +249,14 @@ document.getElementById('cardNumber').addEventListener('input', function(e) {
 
 
 document.getElementById('cardName').addEventListener('input', function(e) {
-    const maxLength = 15; // Defina o número máximo de caracteres permitidos
+    const maxLength = 15; 
     let valor = e.target.value;
 
-    // Verifica se o comprimento do valor excede o máximo permitido
+    
     if (valor.length > maxLength) {
         valor = valor.slice(0, maxLength);
     }
 
-    // Define o valor limitado no campo
+   
     e.target.value = valor;
 });
